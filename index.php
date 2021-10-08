f9468149f89f4dcf831900be6ef572d887d649d4
JoenelRey <joenelrey.montillano@evsu.edu.ph>
2021-10-08 09:16:58 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html>
+<head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title> Montillano Resume </title>
+            
+        <!-- the link for css (style.css)-->
+        <link rel="stylesheet" href="css/style.css">
+
+        <!-- link for javascript-->
+        <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+
+<body>
+
+<div class="resume">
+   <div class="resume_left">
+     <div class="resume_profile">
+       <center><img src="./assets/images/c.jpg" alt="profile_pic"></center>
+     </div>
+     <div class="resume_content">
+       <div class="resume_item resume_info">
+         <div class="title">
+           <p class="bold">Montillano Joenel Rey</p>
+           <p class="regular">Network Manager</p>
+         </div>
+         <ul>
+           <li>
+             <div class="icon">
+               <i class="fas fa-map-signs"></i>
+             </div>
+             <div class="data">
+               Ormoc City, Leyte <br /> Philippines
+             </div>
+           </li>
+           <li>
+            
+             <div class="data">
+               +639677015921
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-envelope"></i>
+             </div>
+             <div class="data">
+            joenelrey.montillano @evsu.edu.ph
+             </div>
+           </li>
+           <li>
+            
+             
+           </li>
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">Skills</p>
+         </div>
+         <ul>
+           <li>
+             <div class="skill_name">
+               HTML/CSS/JS
+             </div>
+             <div class="skill_progress">
+               <span style="width: 50%;"></span>
+             </div>
+             <div class="skill_per">50%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Programming
+             </div>
+             <div class="skill_progress">
+               <span style="width: 40%;"></span>
+             </div>
+             <div class="skill_per">50%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+              Baker
+             </div>
+             <br><div class="skill_progress"></br>
+               <span style="width: 50%;"></span>
+             </div>
+             <div class="skill_per">50%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Electrician
+             </div>
+             <div class="skill_progress">
+               <span style="width: 60%;"></span>
+             </div>
+             <div class="skill_per">60%</div>
+           </li>
+           
+         </ul>
+       </div>
+       <div class="resume_item resume_social">
+         <div class="title">
+           <p class="bold">Social Media Accounts</p>
+         </div>
+         <ul>
+           <li>
+             <div class="icon">
+               <i class="fab fa-facebook-square"></i>
+             </div>
+             <div class="data">
+               <p class="semi-bold">Facebook</p>
+               <p>Joenel Rey Montillano</p>
+             </div>
+           </li>
+           
+           <li>
+             <div class="icon">
+               <i class="fab fa-youtube"></i>
+             </div>
+             <div class="data">
+               <p class="semi-bold">Youtube</p>
+               <p>joenel@youtube</p>
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fab fa-linkedin"></i>
+             </div>
+             <div class="data">
+               <p class="semi-bold">Linkedin</p>
+               <p>joenelreymontillano338@gmail.com</p>
+             </div>
+           </li>
+         </ul>
+       </div>
+     </div>
+  </div>
+  <div class="resume_right">
+    
+    <div class="resume_item resume_work">
+        <div class="title">
+           <p class="bold">Personal Information</p>
+         </div>
+        <ul>
+            <li>
+                <div class="info">
+                     <p class="semi-bold">Birthdate</p> 
+                  <p>February 03, 2000</p>
+                </div>
+            </li>
+            <li>
+                <div class="info">
+                     <p class="semi-bold">Address</p> 
+                  <p>Brgy. R.M.Tan, Ormoc City </p>
+                </div>
+            </li>
+            <li>
+                <div class="info">
+                     <p class="semi-bold">Gender</p> 
+                  <p>Male </p>
+                </div>
+            </li>
+            <li>
+                <div class="info">
+                     <p class="semi-bold">Civil Status</p> 
+                  <p>Single </p>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_education">
+      <div class="title">
+           <p class="bold">Educational Background</p>
+         </div>
+      <ul>
+            <li>
+                <div class="date">2002 - 2012</div> 
+                <div class="info">
+                     <p class="semi-bold">Leondoni Elementary School</p> 
+                  <p>Graduated at Leondoni Elementary School</p>
+                </div>
+            </li>
+            <li>
+              <div class="date">2012 - 2016</div>
+              <div class="info">
+                     <p class="semi-bold">New Ormoc City National High School </p> 
+                  <p>Graduated at New Ormoc City National High School </p>
+                </div>
+            </li>
+            <li>
+              <div class="date">2016 - 2018</div>
+              <div class="info">
+                     <p class="semi-bold">Ormoc City Senior High School </p> 
+                  <p>Graduated at Ormoc City Senior High School </p>
+                </div>
+            </li>
+            <li>
+              <div class="date">2018 - Present</div>
+              <div class="info">
+                     <p class="semi-bold">Eastern Visayas State University </p> 
+                  <p>Currently Studying at Eastern Visayas State University </p>
+                </div>
+            </li>
+        </ul>
+    </div>
+    
+    </div>
+  </div>
+</div>
+
+</body>
+</html> 
+
